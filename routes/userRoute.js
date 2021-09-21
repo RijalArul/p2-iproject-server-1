7478b9509164af24da3fd9649575daded8f902ed
RijalArul <fakhrulmrijal7599@gmail.com>
2021-09-21 14:09:59 +0700

    initialize server

--- routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const UserController = require('../controllers/userController')
-const userRoute = express.Router()
-
-userRoute.post(`/register`, UserController.register)
-userRoute.post(`/login`, UserController.login)
-
-module.exports = userRoute
